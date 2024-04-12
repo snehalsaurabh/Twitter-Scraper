2d2445497c5af8d6bee8aa2454d4dd150e811d75
Snehal Saurabh <snehalsaurabh.code@gmail.com>
2024-04-13 04:25:52 +0530

    Completely scraped dataset

---
+++ scraper.py
@@ -0,0 +1,32 @@
+from ntscraper import Nitter
+import pandas as pd
+import time  # Import time module for adding delays
+
+# List of hashtags
+hashtags = ['news', 'burglary', 'terroristattack', 'fire', 'accident', 'war', 'riot', 'curfew', 'pandemic']
+
+# Initialize an empty list to store tweets
+all_tweets = []
+
+# Specify the Nitter instance
+nitter_instance = "https://nitter.net"
+
+# Iterate over hashtags
+for hashtag in hashtags:
+    # Initialize Nitter scraper
+    scraper = Nitter(log_level=1)
+
+    # Get tweets for the current hashtag using the specified instance
+    tweets = scraper.get_tweets(hashtag, mode="term", number=100, instance=nitter_instance)
+
+    # Extend the list of all_tweets with tweets for the current hashtag
+    all_tweets.extend(tweets['tweets'])
+
+    # Add a delay before making the next request
+    time.sleep(5)  # Add a delay of 5 seconds between requests
+
+# Convert all_tweets list to a DataFrame
+df = pd.DataFrame(all_tweets)
+
+# Save DataFrame to CSV file
+df.to_csv("dataset_1.csv", index=False)
